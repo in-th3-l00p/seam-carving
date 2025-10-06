52dde7e9031a7a013e57b9bb09b36cc48cdca25f
Tisca Catalin <admin@tiscacatalin.com>
2025-10-06 23:31:10 +0300

    streamlit hello world

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
-print("hello world")
+import streamlit as st
+
+st.set_page_config(
+    page_title="seam carving demo",
+    page_icon="🖼️",
+    layout="centered",
+)
+
+st.title("seam carving demo")
+st.write("hello streamlit, hope you won't give me headaches")
